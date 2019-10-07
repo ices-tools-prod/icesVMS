0e128441baeb13c8797de5d35ece7779c032c153
Colin Millar <colin.millar@ices.dk>
2019-10-07 14:29:35 +0200

    * add get_fo_landings()

---
+++ R/get_fo_landings.R
@@ -0,0 +1,24 @@
+#' Download fishing landings summaries
+#' 
+#' Download a data.frame of total weight by country and year for a
+#' given ICES ecoregion.
+#' 
+#' @param ecoregion ICES ecoregion
+#' 
+#' @return a data.frame
+#' 
+#' @export
+get_fo_landings <- function(ecoregion) {  
+  url <- 
+    httr::parse_url(
+      paste0("https://taf.ices.dk/vms/api/fisheriesoverviews/landings/", 
+             utils::URLencode(ecoregion)))
+
+  url <- httr::build_url(url)
+
+  out <- httr::GET(url)
+
+  message(paste("status code:", httr::status_code(out)))
+
+  httr::content(out, simplifyVector = TRUE)
+}
