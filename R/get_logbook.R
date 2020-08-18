1ffa30b63cd1566718030561b84f134f36d61859
colinpmillar <colin.millar@ices.dk>
2020-08-18 09:34:01 +0200

    * add get_logbook() to download logbook data

---
+++ R/get_logbook.R
@@ -0,0 +1,40 @@
+#' Download Logbook data
+#'
+#' RESTRICED.  Only core members of the ICES VMS datacall can acess this data.
+#' Download a data.frame of VMS data from the ICES VMS and logbook database.
+#'
+#' @param country country code
+#' @param year integer year
+#' @param month integer month
+#' @param gear_code benthis gear code
+#' @param metier level 6 metier code
+#' @param stat_rec ICES statistical rectangle
+#' @param ices_area ICES area
+#' @param ecoregion ICES ecoregion
+#'
+#' @return a data.frame of VMS data
+#' @export
+get_logbook <- function(country, year, month,
+                    gear_code, metier,
+                    stat_rec, ices_area, ecoregion) {
+  url <- httr::parse_url("https://taf.ices.dk/vms/api/logbook")
+
+  args <- lapply(as.list(match.call())[-1], eval, parent.frame())
+  if (any(sapply(args, length) > 1)) {
+    args_grid <- do.call(expand.grid, c(args, KEEP.OUT.ATTRS = FALSE, stringsAsFactors = FALSE))
+
+    args_list <- lapply(1:nrow(args_grid), function(i) as.list(args_grid[i, ]))
+
+    message("running ", length(args_list), " calls to get_vms().")
+    out <- lapply(args_list, function(args) do.call(get_vms, args))
+
+    return(do.call(rbind, out))
+  }
+
+  url$query <- args
+  url <- httr::build_url(url)
+
+  out <- vms_get(url)
+
+  httr::content(out, simplifyVector = TRUE)
+}
