4f8a000c6d23e8b1821ad6a9e5932cf07d057e2f
Colin Millar <colin.millar@ices.dk>
2020-10-27 13:53:04 +0100

    * add function get_upload_summary()

---
+++ R/get_upload_summary.R
@@ -0,0 +1,21 @@
+#' Download a summary of submitted data
+#'
+#' Download a summary of submitted data
+#'
+#' @param datacall integer year giving which data call year to enquire about.
+#'   If NULL returns the a summary of the most recent approved data.
+#'
+#' @return a data.frame of VMS summary data
+#' @export
+get_upload_summary <- function(year = NULL) {
+
+  url <- httr::parse_url("https://taf.ices.dk/vms/api/vmssummary")
+
+  if (!is.null(datacall)) {
+    url <- httr::parse_url(sprintf("https://taf.ices.dk/vms/api/vmssummary/%i", datacall))
+  }
+
+  out <- vms_get(url)
+
+  httr::content(out, simplifyVector = TRUE)
+}
