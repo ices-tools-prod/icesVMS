6ee2bd50b66012419fefe97a58158c6a4d2e295e
Colin Millar <colin.millar@ices.dk>
2022-01-25 21:15:59 +0100

    * add datacall argument to wgfbit functions

--- R/get_wgfbit_data1.R
+++ R/get_wgfbit_data1.R
@@ -7,6 +7,8 @@
 #' @param year which year to select
 #' @param fishing_category optional gear category ("Otter", "Dredge")
 #' @param benthis_metier optional benthis metier ("SDN_DMF")
+#' @param datacall integer year giving which data call year to enquire about.
+#'   If NULL returns the a summary of the most recent approved data.
 #'
 #' @return a data.frame with a WKT column for the c-square polygons
 #'
@@ -16,18 +18,24 @@
 #' are supplied the total is calculated.
 #'
 #' @export
-get_wgfbit_data1 <- function(ecoregion, year, fishing_category = NULL, benthis_metier = NULL) {
+get_wgfbit_data1 <- function(ecoregion, year, fishing_category = NULL, benthis_metier = NULL, datacall = NULL) {
   url <-
     httr::parse_url(
       paste0("https://taf.ices.dk/vms/api/wgfbit/dataset1/",
              utils::URLencode(ecoregion), "/", year))
 
+  args <- list()
   if (!is.null(fishing_category)) {
-    url$query <- list(fishing_category = fishing_category)
+    args <- list(fishing_category = fishing_category)
   } else
   if (!is.null(benthis_metier)) {
-    url$query <- list(benthis_metier = benthis_metier)
+    args <- list(benthis_metier = benthis_metier)
   }
+
+  if (!is.null(datacall)) {
+    args <- c(args, list(datacall = datacall))
+  }
+  url$query <- args
 
   # warn if both gear_group and benthis_metier are supplied
   if (!is.null(fishing_category) & !is.null(benthis_metier)) {
