c06b6132cd1b9a25b5c675140631b8541f3a4856
Colin Millar <colin.millar@ices.dk>
2023-04-18 19:28:22 +0200

    add checks prior to submission

--- R/screen_vms_file.R
+++ R/screen_vms_file.R
@@ -6,8 +6,9 @@
 #' needs to be authenticated in order to call this service.
 #' This file can be later pushed to the database by the same user.
 #'
-#' @param file file name of the file containing the data to screen
+#' @param filename file name of the file containing the data to screen
 #' @param verbose return verbose information about the POST request
+#' @param force force submission even if checks to do not pass
 #'
 #' @return text message from the screening process
 #'
@@ -16,20 +17,61 @@
 #' # requires authorization
 #' filename <- system.file("test_files/vms_test.csv", package = "icesVMS")
 #' screen_vms_file(filename)
+#'
+#' filename <- system.file("test_files/vms_test_ok.csv", package = "icesVMS")
+#' screen_vms_file(filename)
 #' }
 #'
+#' @importFrom icesDatsuQC runQCChecks runVocabChecks
 #' @importFrom httr upload_file
-#' @importFrom httr content
 #' @export
-screen_vms_file <- function(file, verbose = FALSE) {
+screen_vms_file <- function(filename, verbose = FALSE, force = FALSE) {
+  type <- scan(filename, "", n = 1, sep = ",", quiet = TRUE)
+  message(glue("Assuming record type is {type}"))
+
+  if (verbose) {
+    suppressMessages <- function(x) x
+  }
+  # run local vocab checks
+  vc <- suppressMessages(runVocabChecks(filename, 145, type))
+
+  # run local QC checks
+  qc <- suppressMessages(runQCChecks(filename, 145, type))
+
+  if (length(vc) > 0) {
+    cat("Summary of Vocab errors:\n")
+    print(vc)
+  }
+
+  if (!is.null(qc)) {
+    cat("Summary of QC failures:\n")
+    print(qc)
+  }
+
+  if (length(vc) > 0 && !is.null(qc)) {
+    warning(
+      glue(
+        "{length(vc)} vocab and {length(qc)} quality checks did not pass."
+      )
+    )
+    if (!force) {
+      warning("File NOT sent to DATSU!")
+      out <- list(vocab = vc, quality = qc)
+      return(invisible(out))
+    }
+  }
+
   url <- "https://data.ices.dk/vms/webapi/ScreenVMSFile"
 
   body <-
     list(
-      fileToScreen = upload_file(file)
+      fileToScreen = upload_file(filename)
     )
 
-  resp <- vms_post(url, body, verbose = verbose, use_token = TRUE)
+  resp <- suppressMessages(vms_post(url, body, verbose = verbose, use_token = TRUE))
 
-  content(resp)
+  msg <- content(resp)
+
+  
+  msg
 }
